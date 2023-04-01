7bdaf2b5f5ce9cceb1a7c7b1350329d1e8a74857
aclongo <125140476+aclongo@users.noreply.github.com>
2023-04-01 13:37:53 -0400

    Import re and apply regex to each line of file

--- grep_simulator.py
+++ grep_simulator.py
@@ -5,12 +5,14 @@
     # Enter a regular expression: ^Author
     # mbox.txt had 1798 lines that matched ^Author
 
+import re
+
 # Ask user for a txt file name and save in fname variable
 while True:
     try:    
         fname = input('Enter the name of a txt file: ')
     # Open the txt file with a file handle
-        hand = open(fname)
+        fhand = open(fname)
         break
     # Use try and except to prevent bad file names
     except FileNotFoundError:
@@ -21,6 +23,9 @@ while True:
 regex = input('Enter a regular expression: ')
 # Initialize a line counter
 line_count = 0
+
 # Perform the regex on each line using a for loop
+for line in fhand:
+    match = re.findall(regex, line)
 # Increment the line counter when matches are found (list not empty)
 # Print an f-string using file name, line counter and regex variables
